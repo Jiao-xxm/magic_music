cac89a689b1bd6312759d7428c6cc2eef9eb6305
reinstalljdk <reinstalljdk@192.168.1.196>
2026-01-05 21:42:22 +0800

    init porgram

---
+++ src/main/java/com/yuhan/magic_music/MagicMusicApplication.java
@@ -0,0 +1,13 @@
+package com.yuhan.magic_music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagicMusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuhan/magic_music/MagicMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuhan.magic_music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
